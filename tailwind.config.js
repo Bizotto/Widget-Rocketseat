7512a9d370069d77b282a67f90fdc9c9f94461da
Bizotto <bizotto.nicolas@gmail.com>
2022-06-09 10:49:00 -0300

    Tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      colors:{
-        brand:{
-          500: '#8257e6'
-        }
+      colors: {
+        brand: {
+          300: '#996dff ',
+          500: '#8257e6',
+        },
+      },
+      borderRadius:{
+        md:'4px',
       }
     },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
+};
