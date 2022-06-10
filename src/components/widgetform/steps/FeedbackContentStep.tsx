ce15917f496bf3673e45eb493d984017f081a44a
Bizotto <bizotto.nicolas@gmail.com>
2022-06-10 15:39:09 -0300

    added lock on button

--- src/components/widgetform/steps/FeedbackContentStep.tsx
+++ src/components/widgetform/steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from 'phosphor-react';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../closeButton';
 import { ScreenshotButton } from '../ScreenshotButton';
@@ -15,7 +15,15 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
 
+  const [comment, setComment] = useState('');
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(screenshot, comment);
+  }
 
   return (
     <>
@@ -39,13 +47,17 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form className="my-4 w-full focus:outline-none">
+      <form
+        onSubmit={handleSubmitFeedback}
+        className="my-4 w-full focus:outline-none"
+      >
         <textarea
           className=" min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 
           text-zinc-100 bg-transparent rounded-md focus:border-brand-500 
           focus:ring-offset-brand-500 border-zinc-600 focus:ring-1 focus:ring-brand-500 resize-none 
           focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte-nos detalhes do que está acontecendo..."
+          onChange={(event) => setComment(event.target.value)}
         />
         <footer className="flex gap-2 mt-2">
           <ScreenshotButton
@@ -57,7 +69,9 @@ export function FeedbackContentStep({
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 
             flex justify-center items-center text-sm hover:bg-brand-300 
             focus:outline-none focus:ring-2 focus:ring-offset-zinc-900 
-            focus:ring-brand-500 transition-colors"
+            focus:ring-brand-500 transition-colors disabled:opacity-50 
+            disabled:hover:bg-brand-500"
+            disabled={comment.length === 0}
           >
             Envias o Feedback??!!
           </button>
